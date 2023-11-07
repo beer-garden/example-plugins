234b3f366eb148d50187a53486017a30c26e6c97
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2023-11-07 20:18:08 +0000

    Adding Pub/Sub examples

---
+++ publisher/publisher/__main__.py
@@ -0,0 +1,28 @@
+import logging
+
+from brewtils import PublishClient, Plugin, command
+
+__version__ = "3.0.0.dev0"
+
+class Publisher:
+
+    def __init__(self):
+        self.publishClient = publishClient
+
+    @command()
+    def publish_topics(self, payload:dict, topic:str = "topic1") -> dict:
+        return dict
+
+def main():
+    plugin = Plugin(
+        name="publisher",
+        version=__version__,
+        description="A plugin that publishes to topics",
+        max_concurrent=1,
+    )
+    plugin.client = Publisher()
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ subscribe/subscribe/__main__.py
@@ -0,0 +1,33 @@
+import logging
+
+from brewtils import subscribe, Plugin
+
+__version__ = "3.0.0.dev0"
+
+class SubscribeClient:
+
+    @subscribe(topics=["topic1","topic2"])
+    def subscribe_multiple_topics(self, payload:dict) -> dict:
+        return dict
+
+    @subscribe(topics="topic1")
+    def subscrib_one_topics(self, payload:dict) -> dict:
+        return dict
+
+    @subscribe(topics="topic.*")
+    def subscribe_wildcard_topics(self, payload:dict) -> dict:
+        return dict
+
+def main():
+    plugin = Plugin(
+        name="subscribe",
+        version=__version__,
+        description="A plugin that subscribes to topics",
+        max_concurrent=1,
+    )
+    plugin.client = SubscribeClient()
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
